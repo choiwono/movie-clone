c6afd7e50fffc00cf96385223bf6754be7eca42d
choiwono <treasure_b@naver.com>
2020-03-17 23:59:56 +0900

    :sparkles: landing page && mainImaga 파일 작성

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '99f16d9f5e6c24c423405d93ca6746e8';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
