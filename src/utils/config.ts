a2bb692ffaff1779f7a847853130d897036bd113
choiwono <treasure_b@naver.com>
2020-04-07 23:48:40 +0900

    :art: 상수값 추가

---
+++ src/utils/config.ts
@@ -0,0 +1,12 @@
+export const API_KEY = '99f16d9f5e6c24c423405d93ca6746e8';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const IMAGE_PATH = 'https://image.tmdb.org/t/p/';
+
+export const imageSize = {
+    orginal : `${IMAGE_PATH}original`,
+    w500 : `${IMAGE_PATH}w500`,
+    w780 : `${IMAGE_PATH}w780`,
+};
+
+export const langauge = `&language=ko-KR`;
