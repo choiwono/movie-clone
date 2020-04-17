358e06091b7500075337a15f0d4c00b96ac0617c
choiwono <choiwono@t3q.com>
2020-04-17 09:39:58 +0900

    :art: shared 폴더로 분리

---
+++ src/pages/Movies/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactFragment, Fragment } from 'react';
+// popular, trending
+interface Props {
+
+}
+
+const Movies: React.FC<Props> = props => {
+    const { } = props;
+
+    return (
+        <Fragment>
+            무비
+        </Fragment>
+    );
+};
+
+export default Movies;
